95a6a7ae51cb3405304f60a55d96a18012a96b71
Eric Kampman <erickampman@yahoo.com>
2017-06-11 07:17:34 -0700

    Initial Commit

---
+++ HelpTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelpTest
+//
+//  Created by Eric Kampman on 6/11/17.
+//  Copyright © 2017 Eric Kampman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
